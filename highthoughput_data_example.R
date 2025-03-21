53bdd0d72e08698b8fd6dcf2e78ce25df796959c
Akanksha Pandey <aakanksha.vit@gmail.com>
2025-03-21 08:59:03 -0400

    "Added R Refresher examples"

--- highthoughput_data_example.R
+++ highthoughput_data_example.R
@@ -29,7 +29,29 @@ geneExpression[,GSM136727.CEL.gz]
 
 my_dataframe <- as.data.frame(geneExpression)
 
+pid = geneAnnotation$PROBEID[which(geneAnnotation$SYMBOL== 'ARPC1A')]
+samDate = sampleInfo$filename[(grep('2005-06-10', sampleInfo$date))]
+
+geneExpression[pid,samDate]
+
+firstMed = apply(geneExpression, 2, median)
+secMed = median(firstMed)
+secMed
 
 
+tpval = function(e, group){
+  group = as.factor(group)
+  pVal = t.test(e[group==1], e[group == 0])$p.value
+  return(pVal)
+}
 
+e = rnorm(20)
+g0 = as.factor(sample(c(0,1), length(e), replace = T))
+# t.test(e[g0==1], e[g0 == 0])$pvalue
 
+res = tpval(e, g0)
+res
+
+g = g <- factor(sampleInfo$group)
+
+sort(apply(geneExpression,1,tpval, g))[1]
